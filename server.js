07ae08ce9174664fb9da033b7ccc74e55bec758e
limfiq <mtaufiq39@gmail.com>
2023-06-11 19:32:56 +0700

    Menambahkan pemanggilan route pada server.js

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const app = express();
 
+//parse json
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
+
+//panggil route
+var routes=require('.route');
+routes(app);
+
 app.listen( 3000, () =>
 {
     console.log( 'Server Berjalan di Port 3000' );
