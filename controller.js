5356ce0f083d8a42eb63cbcef97005402263d112
limfiq <mtaufiq39@gmail.com>
2023-06-13 11:04:36 +0700

    Sudah dapat menampilkan semua data

--- controller.js
+++ controller.js
@@ -5,3 +5,17 @@ var connection=require('./koneksi');
 exports.index=function(req,res){
     response.ok("REST API Berjalan",res);
 }
+
+exports.tampilkansemua = function ( req, res )
+{
+    connection.query( 'select * from mahasiswa', function ( error, rows, field )
+    {
+        if ( error )
+        {
+            connection.log( error );
+        } else
+        {
+            response.ok( rows, res )
+        }
+    } ); 
+}
